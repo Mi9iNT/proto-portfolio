8faeca2cd1166fe758c5083c8c186712aca24735
Mi9iNT <72730181+Mi9iNT@users.noreply.github.com>
2022-08-02 10:47:14 +0200

    va falloir fixer les webkits

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,9 +4,9 @@ import { NavLink } from 'react-router-dom';
 const NotFound = () => {
     return (
         <div className="notFound">
-            <h2>404</h2>
-            <h3>Not Found</h3>
-            <h4>Bruh...</h4>
+            <h2 className='NotFoundTitle'>404</h2>
+            <h3 className='notFound'>Not Found</h3>
+            <h4 className='bruh'>Bruh...</h4>
             <NavLink to="/">
               <button className="Btn-404-Me-Main">Revenir</button>
             </NavLink>
